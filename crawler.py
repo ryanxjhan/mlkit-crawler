32ccd7db39784764b7716c4ba99e3f4e0d9de516
Ryan Han <ryan.han@uwaterloo.ca>
2019-04-08 23:56:25 -0400

    version 0.0.1

---
+++ crawler.py
@@ -0,0 +1,116 @@
+import imghdr
+import os
+import shutil
+from multiprocessing import Pool
+
+import requests
+
+from links import Links
+
+
+class GoogleImageCrawler:
+    def __init__(self, skip=True, n_threads=4, path=""):
+        self.skip = skip
+        self.n_threads = n_threads
+        self.path = path
+    
+    def report(self):
+        pass
+    
+    def crawl(self):
+        keywords = self.keywords()
+        tasks = []
+
+        for keyword in keywords:
+            dir_name = '{}/{}'.format(self.path, keyword)
+            if os.path.exists(os.path.join(os.getcwd(), dir_name)) and self.skip:
+                print('Skipping directory already existing{}.'.format(dir_name))
+                continue
+
+        tasks.append(keyword)
+  
+        pool = Pool(self.n_threads)
+        pool.map_async(self.download, tasks)
+        pool.close()
+        pool.join()
+        print('Task ended. Pool join.')
+
+        self.report()
+
+        print('End Program')
+    
+    @staticmethod
+    def get_extension(link, default='jpg'):
+        splits = str(link).split('.')
+        if len(splits) == 0:
+            return default
+        ext = splits[-1].lower()
+        if ext == 'jpg' or ext == 'jpeg':
+            return 'jpg'
+        elif ext == 'gif':
+            return 'gif'
+        elif ext == 'png':
+            return 'png'
+        else:
+            return default
+
+    @staticmethod
+    def save(response, path):
+        pass
+
+    @staticmethod
+    def validate(path):
+        ext = imghdr.what(path)
+        if ext == 'jpeg':
+            ext = 'jpg'
+        return ext
+
+    def download(self, keyword):
+        source = Links()
+        print('Collecting downloadable links of {}...'.format(keyword))
+        links = source.collect(keyword)
+
+        print('Downloading images of {} from collected links...'.format(keyword))
+        self.mkdir('{}/{}'.format(self.path, keyword))
+        n_links = len(links)
+        for index, link in enumerate(links):
+            try:
+                print('Downloading this image based on the keyword {} from {}: {}/{}'.format(keyword, link, index + 1, n_links))
+                response = requests.get(link, stream=True)
+                ext = self.get_extension(link)
+                raw_path = '{}/{}/{}'.format(self.path, keyword, str(index).zfill(4))
+                path = raw_path + '.' + ext
+                self.save(response, path) 
+
+                del response
+
+                print("Validating image file")
+                ext2 = self.validate(path)
+                if ext2 is None:
+                    print('Unreadable file - {}'.format(link))
+                    os.remove(path)
+                else:
+                    if ext != ext2:
+                        path2 = raw_path + '.' + ext2
+                        os.rename(path, path2)
+                        print('Renaming extension {} -> {}'.format(ext, ext2))
+            except Exception as e:
+                print('Download failed.', e)
+                continue
+
+    @staticmethod
+    def mkdir(name):
+        current_path = os.getcwd()
+        path = os.path.join(current_path, name)
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    @staticmethod
+    def keywords(file='keywords.txt'):
+        with open(file, 'r', encoding='utf-8-sig') as f:
+            text = f.read()
+            lines = text.split('\n')
+            lines = filter(lambda x: x != '' and x is not None, lines)
+            keywords = sorted(set(lines))
+
+        return keywords
---
+++ links.py
@@ -0,0 +1,94 @@
+import platform
+import time
+
+from selenium import webdriver
+from selenium.common.exceptions import (ElementNotVisibleException,
+                                        StaleElementReferenceException)
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class Links:
+    def __init__(self):
+        executable = ''
+
+        if platform.system() == 'Windows':
+            print('Detected OS : Windows')
+            executable = './chromedriver/chromedriver_win.exe'
+        elif platform.system() == 'Linux':
+            print('Detected OS : Linux')
+            executable = './chromedriver/chromedriver_linux'
+        elif platform.system() == 'Darwin':
+            print('Detected OS : Mac')
+            executable = './chromedriver/chromedriver_mac'
+        else:
+            assert False, 'Unknown OS Type'
+
+        self.browser = webdriver.Chrome(executable)
+
+    def get_scroll(self):
+        pos = self.browser.execute_script("return window.pageYOffset;")
+        return pos
+
+    def wait_and_click(self, xpath):
+        #  Sometimes click fails unreasonably. So tries to click at all cost.
+        try:
+            w = WebDriverWait(self.browser, 15)
+            elem = w.until(EC.element_to_be_clickable((By.XPATH, xpath)))
+            elem.click()
+        except Exception as e:
+            print('Click time out - {}'.format(xpath))
+            print('Refreshing browser...')
+            self.browser.refresh()
+            time.sleep(2)
+            return self.wait_and_click(xpath)
+
+        return elem
+
+    def collect(self, keyword, add_url=""):
+        self.browser.get("https://www.google.com/search?q={}&source=lnms&tbm=isch{}".format(keyword, add_url))
+
+        time.sleep(1)
+
+        print('Scrolling down')
+
+        elem = self.browser.find_element_by_tag_name("body")
+
+        for i in range(60):
+            elem.send_keys(Keys.PAGE_DOWN)
+            time.sleep(0.2)
+
+        try:
+            self.wait_and_click('//input[@id="smb"]')
+
+            for i in range(60):
+                elem.send_keys(Keys.PAGE_DOWN)
+                time.sleep(0.2)
+
+        except ElementNotVisibleException:
+            pass
+
+        photo_grid_boxes = self.browser.find_elements(By.XPATH, '//div[@class="rg_bx rg_di rg_el ivg-i"]')
+
+        print('Scraping links')
+
+        links = []
+
+        for box in photo_grid_boxes:
+            try:
+                imgs = box.find_elements(By.TAG_NAME, 'img')
+
+                for img in imgs:
+                    src = img.get_attribute("src")
+                    if src[0] != 'd':
+                        links.append(src)
+
+            except Exception as e:
+                print('[Exception occurred while collecting links from google] {}'.format(e))
+
+        print('Collect links done. Site: {}, Keyword: {}, Total: {}'.format('google', keyword, len(links)))
+        self.browser.close()
+
+        return set(links)
